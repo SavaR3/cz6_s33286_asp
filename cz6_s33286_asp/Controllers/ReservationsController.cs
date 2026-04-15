5d0a45a481082816df0fb15abcbc500669014baa
Sava Rachynskyi <s33286@pjwstk.edu.pl>
2026-04-15 10:57:38 +0200

    Initial commit

---
+++ cz6_s33286_asp/Controllers/ReservationsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cz6_s33286_asp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+    }
+}
